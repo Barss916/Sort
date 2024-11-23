bcd93d78931cb4b71574c5c0000e132128cd6cb8
ilyas <ilya.surgutskij11@mail.ru>
2024-11-23 15:48:20 +0500

    Add some database models

--- Sort/Parsing/models.py
+++ Sort/Parsing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="media/shops")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    availability = models.BooleanField()
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to="media/products")
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
